150a99e528eb860d03d7c9ae72d45e294ec77c40
Tripp Gordon <jgordo17@uvm.edu>
2020-02-19 14:04:06 -0500

    Started migrating training to functions

--- my_ddsp_utils.py
+++ my_ddsp_utils.py
@@ -7,9 +7,6 @@ warnings.filterwarnings("ignore")
 import librosa, librosa.display #display explicitly, bug https://github.com/librosa/librosa/issues/343
 import IPython.display as ipd # for playing audio in jupyter notebook
 
-import tensorflow.compat.v1 as tf
-print("tf version: ",tf.__version__)
-# tf.compat.v1.enable_v2_behavior()
 DEFAULT_SAMPLE_RATE = 16000
 sample_rate = DEFAULT_SAMPLE_RATE
 
