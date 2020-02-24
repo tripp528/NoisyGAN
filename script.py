3839fc5032d88b022003081c006bce6709635a3a
Tripp <tripp528@gmail.com>
2020-02-23 19:37:26 -0500

    update script flags

--- script.py
+++ script.py
@@ -9,13 +9,13 @@ from ddsp_autoencoder import *
 flags = tf.compat.v1.flags
 flags.DEFINE_boolean("build_records",False,"build tfrecords or use existing ones in model_dir")
 flags.DEFINE_string("model_dir","./models/auto/","model directory")
+flags.DEFINE_string("audio_input","./Data/piano/piano30s.wav","audio file")
+flags.DEFINE_string("record_pattern",'./Data/piano/piano30s.tfrecord',"where to put the tfrecords")
 flags.DEFINE_integer("iters", 30, "number iterations to train model")
 opt = flags.FLAGS
 
 # preprocess into tfrecords
-input_audio_filepattern = "./Data/piano/piano30s.wav"
-output_tfrecord_path = './Data/piano/piano30s.tfrecord'
-dataset = DDSP_DATASET(input_audio_filepattern, output_tfrecord_path,buildRecords=opt.build_records)
+dataset = DDSP_DATASET(opt.audio_input, opt.record_pattern,buildRecords=opt.build_records)
 
 #build model
 logging.set_verbosity(logging.INFO)
