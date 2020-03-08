eb434200b497ef64541935bafd9fc473165bf14f
Tripp Gordon <jgordo17@uvm.edu>
2020-03-08 17:59:49 -0600

    got script/train.py working with relative imports. must run with command python -B -m scripts.train

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -17,7 +17,7 @@
 # %x=job-name %j=jobid
 #SBATCH --output=./models/console/%x_%j.out
 # change to the directory where you submitted this script
-cd ${SLURM_SUBMIT_DIR}
+cd ${SLURM_SUBMIT_DIR}/..
 # your job execution follows:
 source activate ddsp
-time python ~/scratch/NoisyGAN/script.py --iters=1 --model_dir=./models/nsynth-auto/  --tfrecord_pattern=./Data/NSynth/val/flute_acoustic_002.tfrecord
+time python -m scripts.train
--- scripts/train.py
+++ scripts/train.py
@@ -1,11 +1,12 @@
-from .ddsp_trainer import *
+# from core import *
+from core.ddsp_trainer import *
 
 # get flags from command line
 flags = tf.compat.v1.flags
 flags.DEFINE_string("model_dir","./models/auto/","model directory")
 flags.DEFINE_string("audio_input",None,"audio file")
 flags.DEFINE_string("tfrecord_pattern",'./Data/piano/piano30s.tfrecord',"where to put the tfrecords")
-flags.DEFINE_integer("iters", 10000, "number iterations to train model")
+flags.DEFINE_integer("iters", 10, "number iterations to train model")
 flags.DEFINE_list("gpus", None, "list of gpu addresses if using multiple")
 flags.DEFINE_boolean("gpu_limit",False,"limit on gpu memory (for linux box")
 opt = flags.FLAGS
@@ -19,6 +20,5 @@ if opt.gpu_limit:
 
 #build model
 train_set = DDSP_DATASET(opt.tfrecord_pattern, audio_input=opt.audio_input)
-model = Solo_Autoencoder()
-trainer = DDSP_TRAINER(model,model_dir=opt.model_dir,gpus=opt.gpus)
-autoencoder.train(train,iterations=opt.iters)
+trainer = DDSP_TRAINER(model_dir=opt.model_dir,gpus=opt.gpus)
+trainer.train(train_set,iterations=opt.iters)
