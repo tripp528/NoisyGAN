e524469ddbdc6698766508c32cae1268f48a5518
Tripp Wallace Gordon <jgordo17@dg-user1.cluster>
2020-03-07 22:08:10 -0500

    submit.sh

---
+++ submit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# specify a partition
+#SBATCH --partition=dggpu
+# Request nodes
+#SBATCH --nodes=1
+# Request some processor cores
+#SBATCH --ntasks=32
+# Request GPUs
+#SBATCH --gres=gpu:8
+# Request memory 
+#SBATCH --mem=16G
+# Maximum runtime of 10 minutes
+#SBATCH --time=10:00
+# Name of this job
+#SBATCH --job-name=mnist_test
+# Output of this job, stderr and stdout are joined by default
+# %x=job-name %j=jobid
+#SBATCH --output=../output/%x_%j.out
+# change to the directory where you submitted this script
+cd ${SLURM_SUBMIT_DIR}
+# your job execution follows:
+source activate ddsp
+time python ~/scratch/NoisyGAN/script.py
