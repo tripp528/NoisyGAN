9c0e83339f5ca649720323fbdb9bb14d0037767e
Tripp Gordon <jgordo17@uvm.edu>
2020-04-27 21:53:55 -0400

    diversity loss was backwards. also added weight param for diversity loss

--- core/gan/gan.py
+++ core/gan/gan.py
@@ -9,7 +9,7 @@ class GAN(Model):
     DEFAULT_ARGS = {
         'batch_size': 8,
         'loss': tf.keras.losses.binary_crossentropy,
-        'add_diversity_loss': False,
+        'diversity_loss_weight': 0,
     }
 
     def __init__(self, **kwargs):
@@ -23,10 +23,11 @@ class GAN(Model):
         classification = self.disc(generated, add_losses=False)
         self.add_loss(self.params["loss"](generated["label"], classification))
 
-        if self.params["add_diversity_loss"]:
+        if self.params["diversity_loss_weight"] > 0:
             std_accross_batch_per_step = tf.math.reduce_std(generated["audio"], axis=0)
             diversity = tf.reduce_mean(std_accross_batch_per_step)
-            diversity = tf.repeat(diversity, repeats=self.params["batch_size"])
-            self.add_loss(diversity)
+            diversity_loss = self.params["diversity_loss_weight"] / diversity
+            diversity_loss = tf.repeat(diversity_loss, repeats=self.params["batch_size"])
+            self.add_loss(diversity_loss)
 
         return classification
